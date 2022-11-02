5eec37c19f8eff1f46da1a270c02bf63356ec919
House-lovers7 <taisuke.got@gmail.com>
2022-11-03 07:18:10 +0900

    File作成

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello world 🍣")
+	_, err := os.Create("./hello,txt")
+	if err != nil {
+		fmt.Printf("failed to create file \n: %v", err)
+		return
+	}
+	fmt.Println("Done!")
 }
+
+
+// package main
+
+// import (
+// 	"flag"
+// 	"fmt"
+// )
+
+// func main() {
+// 	flag.Parse()
+// 	arg := flag.Arg(0)
+// 	fmt.Printf("Hello %s\n", arg)
+// }
