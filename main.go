088c55f1849b6ae06b2d7a4fa88602b0f8a2a307
House-lovers7 <taisuke.got@gmail.com>
2022-11-03 09:55:46 +0900

    出力サイズを指定

--- main.go
+++ main.go
@@ -21,6 +21,9 @@ var favicon = []byte{
 
 func main() {
 	outputPath := flag.String("o", "./image.png", "Path to output file")
+	width := flag.Int("w", 200, "Width of the output image of the QR Code (px)")
+	depth := flag.Int("d", 200, "height of the output image of the QR Code (px)")
+
 	flag.Parse()
 	url := flag.Arg(0)
 	if url == "" {
@@ -33,8 +36,9 @@ func main() {
 		fmt.Printf("file generation failed: %v\n", err)
 		return
 	}
+
 	defer file.Close()
-	img, err := qrgen.GenQRCode(url)
+	img, err := qrgen.GenQRCode(url, *width, *depth)
 	if err != nil {
 		fmt.Printf("image generation failed: %v\n", err)
 		return
